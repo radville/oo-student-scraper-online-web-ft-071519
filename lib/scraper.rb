e16769b9b84d58f349b8a440a329947c088c1910
Laura Radville <laura.radville@gmail.com>
2019-08-03 00:47:17 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,11 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open(index_url))
+    students = doc.css(".student-card")
+    students.each do |student|
+      name = student.css(".student-name")
+    end
   end
 
   def self.scrape_profile_page(profile_url)
--- lib/student.rb
+++ lib/student.rb
@@ -6,6 +6,7 @@ class Student
 
   def initialize(student_hash)
     
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
@@ -17,7 +18,7 @@ class Student
   end
 
   def self.all
-    
+    @@all
   end
 end
 
