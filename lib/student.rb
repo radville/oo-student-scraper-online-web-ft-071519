28f0f7c87345c18bab6874cf3c2f869c61cd80cb
Laura Radville <laura.radville@gmail.com>
2019-08-04 17:21:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,10 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    student = self.new(students_array)
+    students_array.each do |student|
+      student = self.new(student)
+      student.
+    end
   end
 
   def add_student_attributes(attributes_hash)
